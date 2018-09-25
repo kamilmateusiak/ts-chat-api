1fda4432d0669bd411d8f66430f3f35d782b26d9
kamilmateusiak <k.mateusiak@livechatinc.com>
2018-09-25 21:10:18 +0200

    init

---
+++ src/api/auth/index.js
@@ -0,0 +1,20 @@
+import { Router } from 'express'
+import { login } from './controller'
+import { google } from '../../services/passport'
+
+const router = new Router()
+
+/**
+ * @api {post} /auth/google Authenticate with Google
+ * @apiName AuthenticateGoogle
+ * @apiGroup Auth
+ * @apiParam {String} access_token Google user accessToken.
+ * @apiSuccess (Success 201) {String} token User `access_token` to be passed to other requests.
+ * @apiSuccess (Success 201) {Object} user Current user's data.
+ * @apiError 401 Invalid credentials.
+ */
+router.post('/google',
+  google(),
+  login)
+
+export default router
